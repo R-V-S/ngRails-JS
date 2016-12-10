760b5f35028ec1638569a6d78afdadea100ba7e7
Raj <rajeev.v.singh@gmail.com>
2016-12-10 00:02:50 -0500

    Move webpack entry to app/client

---
+++ app/client/app.component.js
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>`,
+})
+export class AppComponent  {
+  constructor() {
+    this.name = 'world!'
+  }
+}
